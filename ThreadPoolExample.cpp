5751b04cffff2d037307bf3d38af72f0dcaf3bac
zemtsovs <nikolay.zemtsovskiy@nokia.com>
2018-09-24 01:47:17 +0300

    added bechmark tests

--- ThreadPoolExample.cpp
+++ ThreadPoolExample.cpp
@@ -1,39 +1,71 @@
 #include <iostream>
+#include <benchmark/benchmark.h>
 #include "ThreadPool.hpp"
 
-
-int main()
+size_t func(size_t n)
 {
-    constexpr size_t size = 16e3;
-    constexpr unsigned int Nsize = 10;
+    size_t ret = 0;
+    for (size_t i = 0; i < 1'000'000; ++i) {
+        benchmark::DoNotOptimize(ret += i * n);
+    }
+
+    return ret;
+};
+
+static void BM_ThreadPool(benchmark::State& state)
+{
+    const size_t size = state.range(0);
+    const unsigned int Nsize = state.range(1);
+    std::vector< std::future<size_t> > results;
+    results.reserve(size);
+    threadUtils::ThreadPool thPool(Nsize);
+
+    for (auto _ : state) {
+        for (size_t i = 0; i < size; ++i) {
+            results.emplace_back( thPool.addTask(func, i * 200) );
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            results[i].wait();
+        }
+    }
+}
+
+BENCHMARK(BM_ThreadPool)->Unit(benchmark::kMillisecond)
+    ->Args({1'000, 1})
+    ->Args({5'000, 1})
+    ->Args({10'000, 1})
+    ->Args({40'000, 1})
+    ->Args({1'000, 2})
+    ->Args({5'000, 2})
+    ->Args({10'000, 2})
+    ->Args({40'000, 2})
+    ->Args({1'000, 4})
+    ->Args({5'000, 4})
+    ->Args({10'000, 4})
+    ->Args({40'000, 4});
+
+static void BM_OneThread(benchmark::State& state)
+{
+    const size_t size = state.range(0);
     std::vector< std::future<size_t> > results;
     results.reserve(size);
 
-    auto func = [] (size_t n) {
-        size_t ret = 0;
-        for (size_t i = 0; i < n; ++i) {
-            ret += i * n;
+    for (auto _ : state) {
+        for (size_t i = 0; i < size; ++i) {
+            results.emplace_back( std::async(std::launch::deferred, func, i * 200) );
         }
 
-        return ret;
-    };
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    threadUtils::ThreadPool thPool(Nsize);
-
-    for (size_t i = 0; i < size; ++i) {
-        results.emplace_back( thPool.addTask(func, i * 200) );
+        for (size_t i = 0; i < size; ++i) {
+            results[i].wait();
+        }
     }
-
-    for (size_t i = 0; i < size; ++i) {
-        results[i].wait();
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-    std::cout << "Elapse time: " << elapsed.count() << std::endl;
-    return 0;
 }
+
+BENCHMARK(BM_OneThread)->Unit(benchmark::kMillisecond)
+        ->Arg(1'000)
+        ->Arg(5'000)
+        ->Arg(10'000)
+        ->Arg(40'000);
+
+BENCHMARK_MAIN();
