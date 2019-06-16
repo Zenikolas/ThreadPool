cd712cbbed022b790b675aea480a8cb2651fe200
Zenikolas <zenikolas256@gmail.com>
2019-06-16 14:08:28 +0300

    got rid off benchmark testing, added manual testing

--- ThreadPoolExample.cpp
+++ ThreadPoolExample.cpp
@@ -1,71 +1,70 @@
 #include <iostream>
-#include <benchmark/benchmark.h>
 #include "ThreadPool.hpp"
 
-size_t func(size_t n)
-{
-    size_t ret = 0;
+using namespace std::chrono;
+
+size_t func(size_t n) {
+    volatile size_t ret = 0;
     for (size_t i = 0; i < 1'000'000; ++i) {
-        benchmark::DoNotOptimize(ret += i * n);
+        ret += i * n + 1;
     }
 
     return ret;
-};
+}
 
-static void BM_ThreadPool(benchmark::State& state)
-{
-    const size_t size = state.range(0);
-    const unsigned int Nsize = state.range(1);
-    std::vector< std::future<size_t> > results;
+template<class Func, class ... Args>
+milliseconds mesureFunc(Func&& func, Args&& ... args) {
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+    std::forward<Func>(func)(std::forward<Args>(args)...);
+
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    return duration_cast<milliseconds>(t2 - t1);
+}
+
+void oneThreadTest(size_t size) {
+    std::vector<size_t> results;
     results.reserve(size);
-    threadUtils::ThreadPool thPool(Nsize);
 
-    for (auto _ : state) {
-        for (size_t i = 0; i < size; ++i) {
-            results.emplace_back( thPool.addTask(func, i * 200) );
-        }
-
-        for (size_t i = 0; i < size; ++i) {
-            results[i].wait();
-        }
+    for (size_t i = 0; i < size; ++i) {
+        results.emplace_back(func(i));
     }
 }
 
-BENCHMARK(BM_ThreadPool)->Unit(benchmark::kMillisecond)
-    ->Args({1'000, 1})
-    ->Args({5'000, 1})
-    ->Args({10'000, 1})
-    ->Args({40'000, 1})
-    ->Args({1'000, 2})
-    ->Args({5'000, 2})
-    ->Args({10'000, 2})
-    ->Args({40'000, 2})
-    ->Args({1'000, 4})
-    ->Args({5'000, 4})
-    ->Args({10'000, 4})
-    ->Args({40'000, 4});
-
-static void BM_OneThread(benchmark::State& state)
-{
-    const size_t size = state.range(0);
-    std::vector< std::future<size_t> > results;
+void threadPoolTest(size_t size, unsigned int threads) {
+    std::vector<std::future<size_t> > results;
     results.reserve(size);
+    threadUtils::ThreadPool thPool(threads);
 
-    for (auto _ : state) {
-        for (size_t i = 0; i < size; ++i) {
-            results.emplace_back( std::async(std::launch::deferred, func, i * 200) );
-        }
+    for (size_t i = 0; i < size; ++i) {
+        results.emplace_back(thPool.addTask(func, i));
+    }
 
-        for (size_t i = 0; i < size; ++i) {
-            results[i].wait();
-        }
+    for (size_t i = 0; i < size; ++i) {
+        results[i].wait();
     }
 }
 
-BENCHMARK(BM_OneThread)->Unit(benchmark::kMillisecond)
-        ->Arg(1'000)
-        ->Arg(5'000)
-        ->Arg(10'000)
-        ->Arg(40'000);
 
-BENCHMARK_MAIN();
+int main() {
+    std::cout << "One thread 1000: " << mesureFunc(&oneThreadTest, 1'000).count() <<
+              std::endl;
+    std::cout << "One thread 5000: " << mesureFunc(&oneThreadTest, 5'000).count() <<
+              std::endl;
+    std::cout << "One thread 10000: " << mesureFunc(&oneThreadTest, 10'000).count() <<
+              std::endl;
+
+    std::cout << "ThreadPool, task size 1000, workers 2: "
+              << mesureFunc(&threadPoolTest, 1'000, 2).count() << std::endl;
+    std::cout << "ThreadPool, task size 5000, workers 2: "
+              << mesureFunc(&threadPoolTest, 5'000, 2).count() << std::endl;
+    std::cout << "ThreadPool, task size 10000, workers 2: "
+              << mesureFunc(&threadPoolTest, 10'000, 2).count() << std::endl;
+
+    std::cout << "ThreadPool, task size 1000, workers 4: "
+              << mesureFunc(&threadPoolTest, 1'000, 4).count() << std::endl;
+    std::cout << "ThreadPool, task size 5000, workers 4: "
+              << mesureFunc(&threadPoolTest, 5'000, 4).count() << std::endl;
+    std::cout << "ThreadPool, task size 10000, workers 4: "
+              << mesureFunc(&threadPoolTest, 10'000, 4).count() << std::endl;
+}
